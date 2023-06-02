0fc21d95c5258618a9c4e1d49ecadc60b9138e42
Anuj Raman <anujramanc@gmail.com>
2023-06-02 12:35:59 +0530

    setting up submit events

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
 
 
 function App() {
 
+  const [inputText, setInputText] = useState("");
+
+  const handleInputChange = (event: HTMLInputElement) => {
+    setInputText(event.target.value);
+  }
+
+  const handleSubmit = (event: HTMLInputElement) => {
+    event.preventDefault();
+    // boolean value true and shows the list component
+    console.log(inputText);
+  }
+
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 style={{ margin: '20px 0px' }}>Grocery Tracker</h2>
         <div className="input-submit">
-          <input type="text" name="text" id="submitText" placeholder="enter something" />
+          <input type="text" value={inputText} onChange={handleInputChange} id="submitText" placeholder="enter something" />
           <input type="submit" value="Submit" />
         </div>
       </form>

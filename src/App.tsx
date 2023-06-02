d9a5d6b42f22f1c9af24c42def9dcca26c66dbc1
Anuj Raman <anujramanc@gmail.com>
2023-06-02 12:27:54 +0530

    basic structure and css

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@
 function App() {
 
   return (
-    <>
-      <div>
-        Initial Commit
-      </div>
-    </>
+    <main>
+      <form>
+        <h2 style={{ margin: '20px 0px' }}>Grocery Tracker</h2>
+        <div className="input-submit">
+          <input type="text" name="text" id="submitText" placeholder="enter something" />
+          <input type="submit" value="Submit" />
+        </div>
+      </form>
+    </main>
   )
 }
 
